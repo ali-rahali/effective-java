21e6e42bb210bf785c3eb6b4b76835b1f2e553c2
Valentine Shemyako <v.shemyako@gmail.com>
2018-10-24 10:53:12 +0300

    Suppress instance creation of utility classes

---
+++ src/main/java/item04/UtilityClass.java
@@ -0,0 +1,20 @@
+package item04;
+
+/**
+ * There're type of classes which contain only static fields and methods and therefore
+ * they are not intended to be instantiated.
+ * <p>
+ * To forbid instance creation of such classes private constructor must be provided.
+ * Also it's possible to create a class final (not actually necessary, but directly denotes
+ * that class is not intended to be subclassed)
+ */
+public final class UtilityClass {
+
+    // suppress creation of class instance
+    private UtilityClass() {
+        // defend against reflection
+        throw new IllegalStateException();
+    }
+
+    // some utility methods below
+}
